7489599a4cdcce63362334f6fb722918813a31db
Sylvestre <sylvestre.gug@gmail.com>
2014-12-04 19:01:38 -0500

    settings.py

--- ua_67/urls.py
+++ ua_67/urls.py
@@ -20,9 +20,4 @@ urlpatterns = patterns('',
 
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'media/(?P<path>.*)',
-        'serve',
-        {'document_root':settings.MEDIA_ROOT}), )
+
