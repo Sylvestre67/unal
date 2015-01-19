d6b8f057609f639191bacfc5fc7be5b11d8fa6be
Sylvestre <sylvestre.gug@gmail.com>
2015-01-19 17:34:17 -0500

    Bureau

--- ua_67/settings.py
+++ ua_67/settings.py
@@ -23,21 +23,21 @@ MANAGERS = ADMINS
 
 DATABASES = {
             'default': {
-                #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-                #'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
-                'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-                'NAME': 'dfauleeq5ti7as',                      # Or path to database file if using sqlite3.
+                'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+                'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
+                #'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+                #'NAME': 'dfauleeq5ti7as',                      # Or path to database file if using sqlite3.
                 #The following settings are not used with sqlite3:
-                'USER': 'jgiscegtfxisbk',
-                'PASSWORD': 'UGWXq3K6zu6_esVYBy16viXKCg',
-                'HOST': 'ec2-54-221-249-3.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-                'PORT': '5432',                      # Set to empty string for default.
+                #'USER': 'jgiscegtfxisbk',
+                #'PASSWORD': 'UGWXq3K6zu6_esVYBy16viXKCg',
+                #'HOST': 'ec2-54-221-249-3.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+                #'PORT': '5432',                      # Set to empty string for default.
             }
         }
 
 #Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
