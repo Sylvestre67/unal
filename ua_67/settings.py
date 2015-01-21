5020d883b8cdb84aa695f1ba05c43a56e2ede100
Sylvestre <sylvestre.gug@gmail.com>
2015-01-20 20:16:26 -0500

    Local Variables

--- ua_67/settings.py
+++ ua_67/settings.py
@@ -23,38 +23,23 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-if not os.environ.has_key('DATABASE_URL'):
-    DATABASES = {
-                'default': {
-                        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-                        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
-                        #'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-                        #'NAME': 'dfauleeq5ti7as',                      # Or path to database file if using sqlite3.
-                        #The following settings are not used with sqlite3:
-                        #'USER': 'jgiscegtfxisbk',
-                        #'PASSWORD': 'UGWXq3K6zu6_esVYBy16viXKCg',
-                        #'HOST': 'ec2-54-221-249-3.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-                        #'PORT': '5432',                      # Set to empty string for default.
-                }
-            }
-if os.environ.has_key('DATABASE_URL'):
-    DATABASES['default'] =  dj_database_url.config()
 
-    #DATABASES = {
-                #'default': {
+DATABASES = {
+                'default': {
                         #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
                         #'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
-                 #       'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-                 #       'NAME': 'dfauleeq5ti7as',                      # Or path to database file if using sqlite3.
+                        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+                        'NAME': 'dfauleeq5ti7as',                      # Or path to database file if using sqlite3.
                         #The following settings are not used with sqlite3:
-                 #       'USER': 'jgiscegtfxisbk',
-                 #       'PASSWORD': 'UGWXq3K6zu6_esVYBy16viXKCg',
-                 #       'HOST': 'ec2-54-221-249-3.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-                 #       'PORT': '5432',                      # Set to empty string for default.
-                #}
-            #}
+                        'USER': 'jgiscegtfxisbk',
+                        'PASSWORD': 'UGWXq3K6zu6_esVYBy16viXKCg',
+                        'HOST': 'ec2-54-221-249-3.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+                        'PORT': '5432',                      # Set to empty string for default.
+                }
+            }
 
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 
 
