e872d5aefc8774416229ba176efc23435bc16711
Sylvestre <sylvestre.gug@gmail.com>
2015-03-06 17:47:44 -0500

    removing unnecessary relations on Models

--- Union_1/admin.py
+++ Union_1/admin.py
@@ -1,1 +1,35 @@
-__author__ = 'Sylvestre'
+from django.contrib import admin
+from models import Picture,Member,Friend,Event,Contact_Us,BureauMember
+
+class BureauMemberAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["list_position","__unicode__", "title","picture", "bio"]
+
+class ImageAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["__unicode__", "picture","event", "album", "created"]
+    list_filter = ["event",'album']
+
+
+class MemberAdmin(admin.ModelAdmin):
+    search_fields = ["last_name", "email"]
+    list_display = ["date_membership_application","first_name","last_name","email","city","state"]
+
+class FriendAdmin(admin.ModelAdmin):
+    search_fields = ["last_name", "email"]
+    list_display = ["first_name","last_name","email","city","state"]
+
+class Contact_UsAdmin (admin.ModelAdmin):
+    list_display=["sender","subject","message"]
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ["date"]
+    list_display = ["date","title","description"]
+
+admin.site.register(Picture, ImageAdmin)
+admin.site.register(Member,MemberAdmin)
+admin.site.register(Friend,FriendAdmin)
+admin.site.register(Event,EventAdmin)
+admin.site.register(Contact_Us,Contact_UsAdmin)
+
+admin.site.register(BureauMember, BureauMemberAdmin)
