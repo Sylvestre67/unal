e9fd4fa6309710fbc3e980e643cd965bb00c12b7
Sylvestre67 <sylvestre.gug@gmail.com>
2015-05-06 18:22:43 -0400

    Recification of CSS

---
+++ staticfiles/js/fee.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+ 
+    //console.log('Hello World');
+
+    $('#id_member_type_1').prop('checked', true);
+
+    $.urlParam = function(name){
+    var results = new RegExp('[\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    return results[1] || 0;
+    }
+
+    var type = $.urlParam('type');
+
+
+    if (type == '1') {
+        $('select>option:eq(0)').prop('selected', true);
+        // $('#amount').val('Member');
+        $('#friend').hide();
+
+    }
+    else {
+        $('select>option:eq(1)').prop('selected', true);
+        //$('#amount').val('Friend');
+        $('#member').hide();
+    }
+
+    var action= $.urlParam('action');
+
+    if (action == 'renew'){
+        $('#application').hide();
+    }
+    else {
+        $('#renewal').hide();
+    }
+
+});
